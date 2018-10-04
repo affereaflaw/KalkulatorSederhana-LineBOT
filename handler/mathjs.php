16d536d7bae6e58c431df7d20b0e067dd30a147d
TibTabTub <lombatibtabtub@gmail.com>
2018-10-04 08:13:25 +0700

    kalkulatorfix

--- handler/mathjs.php
+++ handler/mathjs.php
@@ -2,7 +2,7 @@
 
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 
-function calculate($query){
+function hitung($query){
     $query = urlencode($query);
     $result = file_get_contents('http://api.mathjs.org/v4/?expr='.$query);
     $result = new TextMessageBuilder($result);
